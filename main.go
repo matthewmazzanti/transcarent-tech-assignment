02b85f842bc6576f4981ddb630aa380b26bc1bf2
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2021-11-03 13:44:11 -0400

    Add data access/validation and tests

--- main.go
+++ main.go
@@ -2,12 +2,196 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"html"
 	"log"
 	"encoding/json"
 )
 
+type UserPosts struct {
+	Id int `json:"id"`
+	UserInfo User `json:"userInfo"`
+	Posts []Post `json:"posts"`
+}
+
+type User struct {
+	Name string `json:"name"`
+	Username string `json:"username"`
+	Email string `json:"email"`
+}
+
+type Post struct {
+	Id int `json:"id"`
+	Title string `json:"title"`
+	Body string `json:"body"`
+}
+
+var baseUrl = "https://jsonplaceholder.typicode.com"
+
 func main() {
-	log.Println("Hello world!")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	userPosts, err := getUserPosts(10)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	data, err := json.MarshalIndent(userPosts, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(data))
+}
+
+func runServer() {
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getUserPosts(id int) (*UserPosts, error) {
+	user, err := getUser(id)
+	if err != nil {
+		return nil, err
+	}
+
+	posts, err := getPosts(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserPosts{
+		Id: id,
+		UserInfo: user,
+		Posts: posts,
+	}, nil
+}
+
+func getUser(id int) (User, error) {
+	url := fmt.Sprintf("%s/users/%d", baseUrl, id)
+	res, err := getJson(url)
+	if err != nil { return User{}, err }
+
+	return parseUser(res)
+}
+
+func parseUser(res interface{}) (User, error) {
+	data, ok := res.(map[string]interface{})
+	if !ok {
+		return User{}, fmt.Errorf("returned non-object json")
+	}
+
+	name, err := indexStr(data, "name")
+	if err != nil { return User{}, err }
+
+	username, err := indexStr(data, "username")
+	if err != nil { return User{}, err }
+
+	email, err := indexStr(data, "email")
+	if err != nil { return User{}, err }
+
+	return User{
+		Name: name,
+		Username: username,
+		Email: email,
+	}, nil
+}
+
+func getPosts(id int) ([]Post, error) {
+	url := fmt.Sprintf("%s/posts?userId=%d", baseUrl, id)
+	res, err := getJson(url)
+	if err != nil { return nil, err }
+
+	return parsePosts(res)
+}
+
+func parsePosts(res interface{}) ([]Post, error) {
+	data, ok := res.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("non-list json")
+	}
+
+	posts := make([]Post, len(data))
+	for i, postIface := range data {
+		post, err := parsePost(postIface)
+		if err != nil {
+			return nil, err
+		}
+
+		posts[i] = post
+	}
+
+	return posts, nil
+}
+
+func parsePost(res interface{}) (Post, error) {
+	postData, ok := res.(map[string]interface{})
+	if !ok {
+		return Post{}, fmt.Errorf("non-object json")
+	}
+
+	postId, err := indexInt(postData, "id")
+	if err != nil { return Post{}, err }
+
+	title, err := indexStr(postData, "title")
+	if err != nil { return Post{}, err }
+
+	body, err := indexStr(postData, "body")
+	if err != nil { return Post{}, err }
+
+	return Post{
+		Id: postId,
+		Title: title,
+		Body: body,
+	}, nil
+
+}
+
+func indexInt(data map[string]interface{}, key string) (int, error) {
+	valIface, ok := data[key]
+	if !ok {
+		return 0, fmt.Errorf("Data does not contain key: \"%s\"", key)
+	}
+
+	valFloat, ok := valIface.(float64)
+	if !ok {
+		return 0, fmt.Errorf(
+			"Value at key \"%s\" was not a number",
+			key,
+		)
+	}
+
+	val := int(valFloat)
+	if valFloat != float64(val) {
+		return 0, fmt.Errorf(
+			"Number at %f key \"%s\" was not an integer",
+			valFloat,
+			key,
+		)
+	}
+
+	return val, nil
+}
+
+func indexStr(data map[string]interface{}, key string) (string, error) {
+	valIface, ok := data[key]
+	if !ok {
+		return "", fmt.Errorf("Data does not contain key: %s", key)
+	}
+
+	val, ok := valIface.(string)
+	if !ok {
+		return "", fmt.Errorf(
+			"Value at key \"%s\" was not a string",
+			key,
+		)
+	}
+
+	return val, nil
 }
 
 func getJson(url string) (interface{}, error) {
--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
     "reflect"
 )
 
+// User test data
 var expUserStr = `{
   "id": 1,
   "name": "Leanne Graham",
@@ -30,12 +31,19 @@ var expUserStr = `{
   }
 }`
 
+var expUser = User{
+	Name: "Leanne Graham",
+	Username: "Bret",
+	Email: "Sincere@april.biz",
+}
+
 func expUserJson() interface{} {
 	var res interface{}
 	json.Unmarshal([]byte(expUserStr), &res)
 	return res
 }
 
+// Posts test data
 var expPostsStr = `[
   {
     "userId": 1,
@@ -99,14 +107,86 @@ var expPostsStr = `[
   }
 ]`
 
+var expPosts = []Post{
+	{
+		Id: 1,
+		Title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+		Body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+	},
+	{
+		Id: 2,
+		Title: "qui est esse",
+		Body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+	},
+	{
+		Id: 3,
+		Title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+		Body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+	},
+	{
+		Id: 4,
+		Title: "eum et est occaecati",
+		Body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
+	},
+	{
+		Id: 5,
+		Title: "nesciunt quas odio",
+		Body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
+	},
+	{
+		Id: 6,
+		Title: "dolorem eum magni eos aperiam quia",
+		Body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
+	},
+	{
+		Id: 7,
+		Title: "magnam facilis autem",
+		Body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+	},
+	{
+		Id: 8,
+		Title: "dolorem dolore est ipsam",
+		Body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
+	},
+	{
+		Id: 9,
+		Title: "nesciunt iure omnis dolorem tempora et accusantium",
+		Body: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+	},
+	{
+		Id: 10,
+		Title: "optio molestias id quia eum",
+		Body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error",
+	},
+}
+
 func expPostsJson() interface{} {
 	var res interface{}
 	json.Unmarshal([]byte(expPostsStr), &res)
 	return res
 }
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
+
+var expUserPostsStr string = `{
+    "id": 1,
+    "userInfo": {
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz"
+    },
+    "posts": [{
+        "id": 1,
+        "title": "title of the user’s post",
+        "body": "body of the user’s post"
+    }]
+}`
+
+func expUserPostsJson() interface{} {
+	var res interface{}
+	json.Unmarshal([]byte(expUserPostsStr), &res)
+	return res
+}
+
 func TestGetJson(t *testing.T) {
 	user, err := getJson("https://jsonplaceholder.typicode.com/users/1")
 
@@ -128,5 +208,309 @@ func TestGetJson(t *testing.T) {
 	exp = expPostsJson()
 	if !reflect.DeepEqual(exp, posts) {
 		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", exp, posts)
+	}
+}
+
+var indexData = map[string]interface{}{
+	"str": "string",
+	"int": float64(1),
+	"float": float64(1.5),
+	"null": nil,
+	"list": []interface{}{},
+	"obj": map[string]interface{}{},
+}
+
+func TestIndexStr(t *testing.T) {
+	res, err := indexStr(indexData, "str")
+	if err != nil {
+		t.Fatalf("Error while indexing a string %v", err)
+	}
+
+	if res != "string" {
+		t.Fatalf("Got unexpected string: %s", res)
+	}
+
+	_, err = indexStr(indexData, "int")
+	if err == nil { t.Fatalf("Did not get error indexing int") }
+
+	_, err = indexStr(indexData, "float")
+	if err == nil { t.Fatalf("Did not get error indexing float") }
+
+	_, err = indexStr(indexData, "null")
+	if err == nil { t.Fatalf("Did not get error indexing null") }
+
+	_, err = indexStr(indexData, "list")
+	if err == nil { t.Fatalf("Did not get error indexing list") }
+
+	_, err = indexStr(indexData, "obj")
+	if err == nil { t.Fatalf("Did not get error indexing object") }
+}
+
+func TestIndexInt(t *testing.T) {
+	res, err := indexInt(indexData, "int")
+	if err != nil {
+		t.Fatalf("Error while indexing a int %v", err)
+	}
+
+	if res != 1 {
+		t.Fatalf("Got unexpected int: %d", res)
+	}
+
+	_, err = indexInt(indexData, "str")
+	if err == nil { t.Fatalf("Did not get error indexing str") }
+
+	_, err = indexInt(indexData, "float")
+	if err == nil { t.Fatalf("Did not get error indexing float") }
+
+	_, err = indexInt(indexData, "null")
+	if err == nil { t.Fatalf("Did not get error indexing null") }
+
+	_, err = indexInt(indexData, "list")
+	if err == nil { t.Fatalf("Did not get error indexing list") }
+
+	_, err = indexInt(indexData, "obj")
+	if err == nil { t.Fatalf("Did not get error indexing object") }
+
+}
+
+func TestParseUser(t *testing.T) {
+	// Happy path
+	data := map[string]interface{}{
+		"name": "Leanne Graham",
+		"username": "Bret",
+		"email": "Sincere@april.biz",
+		"foo": "bar", // extra field should be ignored
+	}
+
+	exp := User{
+		Name: "Leanne Graham",
+		Username: "Bret",
+		Email: "Sincere@april.biz",
+	}
+
+	user, err := parseUser(data)
+	if err != nil {
+		t.Fatalf("Unexpected error converting user: %v", err)
+	}
+
+
+	if !reflect.DeepEqual(exp, user) {
+		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", exp, user)
+	}
+
+	// Missing fields
+	data = map[string]interface{}{
+		"username": "Bret",
+		"email": "Sincere@april.biz",
+	}
+
+	_, err = parseUser(data)
+	if err == nil {
+		t.Fatalf(
+			"Did not get error parsing data with missing field: %v",
+			data,
+		)
+	}
+
+	// Wrong types
+	data = map[string]interface{}{
+		"name": "Leanne Graham",
+		"username": 10,
+		"email": nil,
+	}
+
+	_, err = parseUser(data)
+	if err == nil {
+		t.Fatalf(
+			"Did not get error parsing data of wrong type: %v",
+			data,
+		)
+	}
+}
+
+func TestParsePost(t *testing.T) {
+	// Happy path
+	data := map[string]interface{}{
+		"id": float64(1),
+		"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+		"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+		"foo": "bar", // extra field should be ignored
+	}
+
+	exp := Post{
+		Id: 1,
+		Title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+		Body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+	}
+
+	post, err := parsePost(data)
+	if err != nil {
+		t.Fatalf("Unexpected error converting user: %v", err)
+	}
+
+
+	if !reflect.DeepEqual(exp, post) {
+		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", exp, post)
+	}
+
+	// Missing fields
+	data = map[string]interface{}{
+		"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+		"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+	}
+
+	_, err = parsePost(data)
+	if err == nil {
+		t.Fatalf(
+			"Did not get error parsing data with missing field: %v",
+			data,
+		)
+	}
+
+	// Wrong types
+	data = map[string]interface{}{
+		"id": float64(1.2),
+		"title": nil,
+		"body": nil,
+	}
+
+	_, err = parsePost(data)
+	if err == nil {
+		t.Fatalf(
+			"Did not get error parsing data of wrong type: %v",
+			data,
+		)
+	}
+}
+
+func TestParsePosts(t *testing.T) {
+	data := []interface{}{
+		map[string]interface{}{
+			"userId": float64(1),
+			"id": float64(1),
+			"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+			"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+		},
+		map[string]interface{}{
+			"userId": float64(1),
+			"id": float64(2),
+			"title": "qui est esse",
+			"body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+		},
+		map[string]interface{}{
+			"userId": float64(1),
+			"id": float64(3),
+			"title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+			"body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+		},
+	}
+
+	exp := []Post{
+		{
+			Id: 1,
+			Title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+			Body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+		},
+		{
+			Id: 2,
+			Title: "qui est esse",
+			Body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+		},
+		{
+			Id: 3,
+			Title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+			Body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+		},
+	}
+
+	posts, err := parsePosts(data)
+	if err != nil {
+		t.Fatalf("Unexpected error converting posts: %v", err)
+	}
+
+	if !reflect.DeepEqual(exp, posts) {
+		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", exp, posts)
+	}
+
+	// Missing field
+	data = []interface{}{
+		map[string]interface{}{
+			"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+			"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+		},
+	}
+
+	_, err = parsePosts(data)
+	if err == nil {
+		t.Fatalf(
+			"Did not get error parsing data with missing field: %v",
+			data,
+		)
+	}
+
+	// Wrong types
+	data = []interface{}{
+		map[string]interface{}{
+			"id": nil,
+			"title": 1.5,
+			"body": []interface{}{},
+		},
+	}
+
+	_, err = parsePost(data)
+	if err == nil {
+		t.Fatalf(
+			"Did not get error parsing data of wrong type: %v",
+			data,
+		)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	user, err := getUser(1)
+	if err != nil {
+		t.Fatalf("Unexpected getting user: %v", err)
+	}
+
+	if !reflect.DeepEqual(expUser, user) {
+		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", expUser, user)
+	}
+}
+
+func TestGetPosts(t *testing.T) {
+	posts, err := getPosts(1)
+	if err != nil {
+		t.Fatalf("Unexpected getting posts: %v", err)
+	}
+
+	if !reflect.DeepEqual(expPosts, posts) {
+		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", expPosts, posts)
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	userPosts := UserPosts{
+		Id: 1,
+		UserInfo: User{
+			Name: "Leanne Graham",
+			Username: "Bret",
+			Email: "Sincere@april.biz",
+		},
+		Posts: []Post{
+			{
+				Id: 1,
+				Title: "title of the user’s post",
+				Body: "body of the user’s post",
+			},
+		},
+	}
+
+	userPostsJson, _ := json.Marshal(userPosts)
+	var userPostsData interface{}
+	json.Unmarshal(userPostsJson, &userPostsData)
+
+	exp := expUserPostsJson()
+	if !reflect.DeepEqual(exp, userPostsData) {
+		t.Fatalf("\nExpected:\n%v\nGot:\n%v\n", exp, userPostsData)
 	}
 }
